b8e194f8168ba2379374830acc52cd58d4848e97
Colin Ho <colin.ho99@gmail.com>
2022-12-20 16:59:17 +0800

    cat-file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 import sys
 import os
+import zlib
 
+def catFile(file_hash):
+    file_path = f'.git/objects/{file_hash[:2]}/{file_hash[2:]}'
+    if os.path.isfile(file_path):
+        with open(file_path, 'rb') as f:
+            data = f.read()
+        raw_bytes = zlib.decompress(data)
+        # account for header, example: blob 16\u0000
+        raw_bytes_without_header = raw_bytes[raw_bytes.index(b'\x00') + 1:]
+        decoded_data = raw_bytes_without_header.decode()
+        print(decoded_data, end='')
 
 def main():
     command = sys.argv[1]
@@ -11,6 +22,8 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        catFile(sys.argv[-1])
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
